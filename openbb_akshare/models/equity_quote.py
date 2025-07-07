abfc761c7dbb439f062bacad8768d3b77389451c
Roger Ye <shugaoye@yahoo.com>
2025-07-07 10:21:15 +0800

    Added HK in equity_quote.py

--- openbb_akshare/models/equity_quote.py
+++ openbb_akshare/models/equity_quote.py
@@ -57,35 +57,40 @@ class AKShareEquityQuoteFetcher(
     ) -> List[Dict]:
         """Extract the raw data from AKShare."""
         # pylint: disable=import-outside-toplevel
-        import asyncio  # noqa
-        from curl_adapter import CurlCffiAdapter
-        from openbb_core.provider.utils.helpers import get_requests_session
         import akshare as ak
+        import pandas as pd
 
         symbols = query.symbol.split(",")
         all_data = []
 
         stock_zh_a_spot_em_df = ak.stock_zh_a_spot_em()
-        stock_quotes = stock_zh_a_spot_em_df[["代码", "名称", "最新价", "今开", "最高", "最低", "涨跌幅", "涨跌额", "成交量", "昨收"]]
-        def get_one(symbol):
+        stock_hk_spot_em_df = ak.stock_hk_spot_em()
+        def get_one(symbol, a_df, hk_df) -> pd.DataFrame:
             """Get the data for one ticker symbol."""
-            quote = stock_quotes[stock_quotes["代码"] == symbol]
+            quote = pd.DataFrame()
+            symbol_b, symbol_f, market = normalize_symbol(symbol)
+            if market == "HK":
+                if not hk_df.empty:
+                    stock_quotes = hk_df[["代码", "名称", "最新价", "今开", "最高", "最低", "涨跌幅", "涨跌额", "成交量", "昨收"]]
+                    quote = stock_quotes[stock_quotes["代码"] == symbol]
+            else:
+                if not a_df.empty:
+                    stock_quotes = a_df[["代码", "名称", "最新价", "今开", "最高", "最低", "涨跌幅", "涨跌额", "成交量", "昨收"]]
+                    quote = stock_quotes[stock_quotes["代码"] == symbol]
+
             if quote.empty:
-                return {"symbol": symbol, "error": "Symbol not found"}
+                return pd.DataFrame([{"symbol": symbol, "error": "Symbol not found"}])
             return quote
 
         for symbol in symbols:
-            try:
-                # Validate symbol format if applicable (e.g., length, allowed characters)
-                if not isinstance(symbol, str) or len(symbol) != 6:
-                    raise ValueError(f"Invalid symbol format: {symbol}")
-                
-                data = get_one(symbol)
+            try:        
+                data = get_one(symbol, stock_zh_a_spot_em_df, stock_hk_spot_em_df)
                 all_data.append(data.to_dict(orient="records")[0])
                 
             except Exception as e:
                 print(f"Error fetching data for symbol {symbol}: {e}")
                 continue
+
         return all_data
 
     @staticmethod
