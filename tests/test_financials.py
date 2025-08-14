ad57a7c7ba078ec908a89b68420450419c2a46a8
Roger Ye <shugaoye@yahoo.com>
2025-08-14 15:29:05 +0800

    Improved test_financials.py

--- tests/test_financials.py
+++ tests/test_financials.py
@@ -1,17 +1,28 @@
+import pytest
 import pandas as pd
 from openbb import obb
 
-def test_balance_sheet(default_provider):
-    balance_df = obb.equity.fundamental.balance(symbol="600325", period="annual", limit=7, provider=default_provider).to_dataframe()
+symbols = ["600325", "600028","600036","600048"]
+
+@pytest.mark.parametrize("symbol", symbols)
+@pytest.mark.parametrize("period_t", ["annual", "quarter"])
+def test_balance_sheet(symbol, period_t, default_provider):
+    balance_df = obb.equity.fundamental.balance(symbol=symbol, period=period_t, limit=7, provider=default_provider).to_dataframe()
     assert not balance_df.empty
     assert len(balance_df) == 7
 
-def test_income(default_provider):
-    income_df = obb.equity.fundamental.income(symbol="600325", limit=3, provider=default_provider).to_dataframe()
+@pytest.mark.parametrize("symbol", symbols)
+@pytest.mark.parametrize("period_t", ["annual", "quarter"])
+def test_income(symbol, period_t, default_provider):
+    income_df = obb.equity.fundamental.income(symbol=symbol, period=period_t, limit=3, provider=default_provider).to_dataframe()
     assert not income_df.empty
     assert len(income_df) == 3
 
-def test_cash_flow(default_provider):
-    cash_flow_df = obb.equity.fundamental.cash(symbol="600325", limit=3, provider=default_provider).to_dataframe()
+@pytest.mark.parametrize("symbol", symbols)
+@pytest.mark.parametrize("period_t", ["annual", "quarter"])
+def test_cash_flow(symbol, period_t, default_provider):
+    cash_flow_df = obb.equity.fundamental.cash(symbol=symbol, period=period_t, limit=3, provider=default_provider).to_dataframe()
     assert not cash_flow_df.empty
     assert len(cash_flow_df) == 3
+
+
